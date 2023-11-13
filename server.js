10f1789347c422e14bf147b8fdac3c4c9b61aaa9
tfkjosh <140848241+tfkjosh@users.noreply.github.com>
2023-11-13 00:10:28 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,63 @@
+// Dependencies & Express Data Parsing
+const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { notes } = require(".//db/db.json");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//Generate Unique ID
+const generateUniqueId = require('generate-unique-id');
+
+
+//Create New Note
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  };  
+
+//Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = generateUniqueId();
+  const note = createNewNote(req.body, notes);
+  res.json(notes);
+});
+    
+//Bonus - DELETE 
+//NOTE: Found this code almost exactly as needed here: https://www.tabnine.com/code/javascript/functions/express/Express/delete
+// I'm probably going to use this a lot in the future because wow what a resource!!
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const delNote = notes.findIndex(note => note.id ==id);
+
+  notes.splice(delNote, 1);
+  return res.send();
+});
+
+//Listener
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
